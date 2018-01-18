32258606f2b20f712083873912c79aafc41e7db1
Jean <jeancarlosrb>
2018-01-17 22:40:07 -0200

    updated on Pages

--- features/support/pages/example.rb
+++ features/support/pages/example.rb
@@ -6,17 +6,19 @@ class Methods
     qtd_tamanho = within('.product-attr-list.attr-size') do
       all('.attr-name').count
     end
-    puts "Tamanho #{qtd_tamanho}"
     #subtrai - 1 dessa variavel para que seja possível chamarmos ela pela posição
     qtd_tamanho = qtd_tamanho - 1
 
     #busco o elemento pela posição -1 e clico nele e valido se esta disponivel
-    all('.attr-name')[qtd_tamanho].click
-
+    within('.product-attr-list.attr-size') do
+      all('.attr-name')[qtd_tamanho].click
+    end
     while has_text?('COMPRAR') == false
       #Enquanto não houver o texto comprar na tela, ele subtrai - 1 da quantidade e tenta o proximo tamanho
       qtd_tamanho = qtd_tamanho - 1
-      all('.attr-name')[qtd_tamanho].click
+      within('.product-attr-list.attr-size') do
+        all('.attr-name')[qtd_tamanho].click
+      end
     end
   end
 end
